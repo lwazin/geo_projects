98bd6b41a3679e1d082dce9ff18c5379358fbfa8
lwazin <nolijndlovu@gmail.com>
2019-02-28 08:16:58 +0200

    Parsing html done.. next, diving into the webpage for some value!

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -1,4 +1,9 @@
 # Importing packages
-import pandas as py
+import pandas as pd
 from bs4 import BeautifulSoup as bs4
 import requests
+
+# Processing webpage for BeautifulSoup
+request = requests.get('https://m.news24.com/Traffic/Gauteng')
+page = request.content
+soup = bs4(page)
