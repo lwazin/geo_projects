8aa7aba8752c25fe2ce8541bed3d0dbd46e8f02f
lwazin <nolijndlovu@gmail.com>
2019-02-28 08:40:02 +0200

    Explored the data within the webpage, picking out what we want.. Now for cleaning it all up!

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -7,3 +7,15 @@ import requests
 request = requests.get('https://m.news24.com/Traffic/Gauteng')
 page = request.content
 soup = bs4(page)
+
+# Finding value within the web page
+# The value we are interested in is placed within list objects, of class 'item clr'
+len(soup.find_all('li', "item clr"))
+# The page returns a list of 10 items.. This is traffic data
+
+# Let's look at the data provided by a single item within this list:
+soup.find_all('li', "item clr")[0].find_all('span', 'location_name')[0].text # Name
+soup.find_all('li', "item clr")[0].find_all('span', 'timestamp')[0].text # TimeStamp
+soup.find_all('li', "item clr")[0].find_all('span', 'description')[0].text # Description
+
+# These values, being a sample of the crude data values we can mine.. Clearly we need to clean it up a bit!
